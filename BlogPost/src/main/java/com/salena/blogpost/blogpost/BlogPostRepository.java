ed0f5331b5ccbb1b9d1143d7367b3799cb142287
salenastamp <73411319+salenastamp@users.noreply.github.com>
2020-11-30 09:00:16 -0500

    Add files via upload

---
+++ BlogPost/src/main/java/com/salena/blogpost/blogpost/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.salena.blogpost.blogpost;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+
+}
---
+++ BlogPost/src/test/java/com/salena/blogpost/BlogPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salena.blogpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
